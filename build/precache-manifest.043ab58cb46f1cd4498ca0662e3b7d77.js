fec25ec162bfd8937716be74a1e6a927d00b7fee
Bunbee <barnabas@velocityinc.tech>
2022-11-23 16:38:05 +0200

    Add react build

---
+++ build/precache-manifest.043ab58cb46f1cd4498ca0662e3b7d77.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8160f73b6802a7295542399592e51d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aabc9bb82011a9c569ec",
+    "url": "/static/css/11.c88e854e.chunk.css"
+  },
+  {
+    "revision": "8bbb37c7da8e836dcb00",
+    "url": "/static/css/12.0afd6408.chunk.css"
+  },
+  {
+    "revision": "0f7f40381b7a6f38ca3f",
+    "url": "/static/css/13.91a59431.chunk.css"
+  },
+  {
+    "revision": "685b605c08534f7150ed",
+    "url": "/static/css/5.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "7061981a842685a6fb76",
+    "url": "/static/css/6.8fb99aa0.chunk.css"
+  },
+  {
+    "revision": "f28ef3fb18d5b1d33b69",
+    "url": "/static/css/7.3e6f92ff.chunk.css"
+  },
+  {
+    "revision": "13f3dee7bcd2bda36461",
+    "url": "/static/css/8.f2a81746.chunk.css"
+  },
+  {
+    "revision": "84d3fe83c83a10721682",
+    "url": "/static/css/9.e3d03609.chunk.css"
+  },
+  {
+    "revision": "82f39bcd16276a91eb8f",
+    "url": "/static/css/main.bfb12ec0.chunk.css"
+  },
+  {
+    "revision": "f16964a8567b8cea5c2f",
+    "url": "/static/js/0.49058ed4.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/0.49058ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2dd4714fc9648eea2e",
+    "url": "/static/js/1.80064715.chunk.js"
+  },
+  {
+    "revision": "e11d73c0da477416aa91",
+    "url": "/static/js/10.3f34446a.chunk.js"
+  },
+  {
+    "revision": "aabc9bb82011a9c569ec",
+    "url": "/static/js/11.89ed227f.chunk.js"
+  },
+  {
+    "revision": "8bbb37c7da8e836dcb00",
+    "url": "/static/js/12.b451f8db.chunk.js"
+  },
+  {
+    "revision": "0f7f40381b7a6f38ca3f",
+    "url": "/static/js/13.9da658a4.chunk.js"
+  },
+  {
+    "revision": "22ca0b6e7a6ac729d8c3",
+    "url": "/static/js/14.20857ccd.chunk.js"
+  },
+  {
+    "revision": "4cb10388f05e87ee8a16",
+    "url": "/static/js/15.ae85254a.chunk.js"
+  },
+  {
+    "revision": "ec71070d95986b193602",
+    "url": "/static/js/16.89992b3f.chunk.js"
+  },
+  {
+    "revision": "5bd5ff3d0038734cb4cb",
+    "url": "/static/js/4.a59e4fb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.a59e4fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685b605c08534f7150ed",
+    "url": "/static/js/5.cd837c32.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/5.cd837c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7061981a842685a6fb76",
+    "url": "/static/js/6.8af28c66.chunk.js"
+  },
+  {
+    "revision": "f28ef3fb18d5b1d33b69",
+    "url": "/static/js/7.62d50feb.chunk.js"
+  },
+  {
+    "revision": "13f3dee7bcd2bda36461",
+    "url": "/static/js/8.b790de08.chunk.js"
+  },
+  {
+    "revision": "84d3fe83c83a10721682",
+    "url": "/static/js/9.e82c8044.chunk.js"
+  },
+  {
+    "revision": "82f39bcd16276a91eb8f",
+    "url": "/static/js/main.ad70d885.chunk.js"
+  },
+  {
+    "revision": "32c8b44d2efec57119c8",
+    "url": "/static/js/runtime-main.c9f02759.js"
+  },
+  {
+    "revision": "dc7a7c89199b94a55c6c18134835df44",
+    "url": "/static/media/new-logo.dc7a7c89.jpg"
+  }
+]);
---
+++ build/static/js/11.89ed227f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpplatinum=this.webpackJsonpplatinum||[]).push([[11],{152:function(e,t,a){"use strict";a.r(t);var n=a(1),l=a.n(n);a(97);t.default=l.a.memo((function(){return l.a.createElement("div",{className:"footer"},l.a.createElement("p",null,"\xa9 2020 Platinum Belts "),l.a.createElement("p",null,"12 palm Blvd Umhlanga Ridge"))}))},97:function(e,t,a){}}]);
+//# sourceMappingURL=11.89ed227f.chunk.js.map
